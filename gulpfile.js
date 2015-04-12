8197895b26d538f2287a5aadcdf995341db7a1f3
TheBeatCoder <shupuercha@gmail.com>
2015-04-12 18:09:00 +0300

    Added table

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	concat = require('gulp-concat'),
+	connect = require('gulp-connect'),
+	uglify = require('gulp-uglify'),
+	sourcemap = require('gulp-sourcemaps');
+
+gulp.task('server', function () {
+	connect.server({
+		root: 'app',
+		port: 1337,
+		livereload: true
+	});
+});
+
+gulp.task('styles', function () {
+	gulp.src('./app/src/styles/main.scss')
+		.pipe(sass())
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest('./app/dist/css'))
+		.pipe(connect.reload());
+});
+
+gulp.task('views', function () {
+	gulp.src('./app/src/views/*.html')
+		.pipe(gulp.dest('./app/dist'))
+		.pipe(connect.reload());
+});
+
+gulp.task('scripts', function () {
+
+})
+
+gulp.task('watch', function () {
+	gulp.watch('./app/src/views/*.html', ['views']);
+	gulp.watch('./app/src/styles/*scss', ['styles']);
+});
+
+gulp.task('test', function () {
+
+});
+
+gulp.task('build',[]);
+
+gulp.task('default', ['server', 'views', 'styles', 'watch']);
